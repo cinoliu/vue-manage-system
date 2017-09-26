288cd6f9a9a905781e98b3c667eab28be78e4932
刘宗宇 <zongyu.liu@foxmail.com>
2017-09-26 13:11:18 +0800

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 let base ='api';
-//let base = 'http://cpre.sweetmartmarketing.com/wechatMember';
 
 export const requestLogin = params => { return axios.post(`${base}/login`, params).then(res => res.data); };
 
